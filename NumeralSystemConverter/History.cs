b400264d5b33cee34723ae070643343329bc6466
wingsmight <wingsmight@gmail.com>
2021-02-21 22:22:17 +0700

    add History

---
+++ NumeralSystemConverter/History.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeralSystemConverter
+{
+    class History
+    {
+        private Stack<Record> records;
+
+
+        public void AddRecord(Record newRecord)
+        {
+            records.Push(newRecord);
+        }
+        public Record PopRecord()
+        {
+            return records.Pop();
+        }
+        public void Clear()
+        {
+            records.Clear();
+        }
+
+
+        public int Count => records.Count;
+    }
+
+    public struct Record
+    {
+        public string sourceNumber;
+        public string resultNumber;
+        public int sourceRadix;
+        public int resultRadix;
+
+
+        public Record(string sourceNumber, string resultNumber, int sourceRadix, int resultRadix)
+        {
+            this.sourceNumber = sourceNumber;
+            this.resultNumber = resultNumber;
+            this.sourceRadix = sourceRadix;
+            this.resultRadix = resultRadix;
+        }
+
+
+        public override string ToString()
+        {
+            return sourceNumber + " [" + sourceRadix + "] -> " + resultNumber + " [" + resultRadix + "]" + Environment.NewLine;
+        }
+    }
+}
